264c4b25ba61a0363a0d6825625f4592d9c9bff9
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2019-04-16 11:24:18 -0400

    ✨ Add missing files

--- copy_modules.sh
+++ copy_modules.sh
@@ -1,4 +1,7 @@
+mkdir -p dist
 cp ./node_modules/mermaid/dist/mermaid.min.js dist/
+cp ./package.json dist/
+cp ./index.html dist/
 
 mkdir -p dist/fontawesome/css/
 cp ./node_modules/@fortawesome/fontawesome-free-webfonts/css/* dist/fontawesome/css/
